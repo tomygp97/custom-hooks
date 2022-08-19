ccc95a355719f0ae49a9f6ffd9389e1c4dec6fae
Tomas <tomasgp1997@gmail.com>
2022-08-18 21:09:08 -0300

    Primer Commit

---
+++ useTodo.js
@@ -0,0 +1,57 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const initialState = [];
+const init = () => {
+    return JSON.parse( localStorage.getItem( "todos" ) ) || [];
+}
+
+export const useTodo = () => {
+
+    const [ todos, dispatch ] = useReducer( todoReducer, initialState, init );
+
+    useEffect(() => {
+
+        localStorage.setItem("todos", JSON.stringify( todos ))
+
+    }, [todos])
+
+    const handleNewTodo = ( todo ) => {
+
+        const action = {
+            type: "[TODO] Add todo",
+            payload: todo,
+        }
+
+        dispatch( action );
+    };
+
+    const handleRemoveTodo = ( id ) => {
+
+        dispatch({
+            type: "[TODO] Remove todo",
+            payload: id,
+        });
+    };
+
+    const handleToggleTodo = ( id ) => {
+
+        dispatch({
+            type: "[TODO] Toggle todo",
+            payload: id,
+        })
+
+    };
+
+    return {
+        todos,
+        todosCount: todos.length,
+        prendingTodosCount: todos.filter( todo => !todo.done ).length,
+        handleNewTodo,
+        handleRemoveTodo,
+        handleToggleTodo,
+    }
+
+
+};
